e842c9636c1193cea29c02b6c7d67aab0230471f
Garrett Steffen <gsteffen@Garretts-MacBook-Pro.local>
2020-01-04 16:31:27 -0500

    Updated scripts to be excecutable outside windows

--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@
 set -ex
 cd "$(dirname $0)"
 
+unameOut="$(uname -s)";
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
 rustup target add wasm32-unknown-unknown --toolchain stable
 
 if ( ! command -v wasm-bindgen )
@@ -15,4 +24,8 @@ cargo build -p server
 
 wasm-bindgen ./target/wasm32-unknown-unknown/debug/game.wasm --out-dir ./www/wasm
 
-cp ./target/debug/server.exe ./server/build
+if [[ "${machine}" == "MinGw" ]] || [[ "${machine}" == "Cygwin" ]]; then
+    cp ./target/debug/server.exe ./server/build
+else
+    cp ./target/debug/server ./server/build
+fi;
